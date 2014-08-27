a92585d57bc615230fd53c47ae12d3c8f5cb7093
Kai Weller <Kai.Weller90@googlemail.com>
2014-08-28 00:21:27 +0200

    Rewrote PoseidonClient; Deleted not needed files; For todos see PoseidonClient.py

--- PoseidonClient/PoseidonClient.py
+++ PoseidonClient/PoseidonClient.py
@@ -1,30 +1,59 @@
-from grove import GrovePi
+import grovepi
 from mqtt import MqttClient
-from sqliteClient import SQLiteClient
-import configurationFile
 import time
-import formater
-
-class PoseidonClient(object):
-    def __init__(self):
-        self.config = configurationFile.poseidonSettings
-        self.grove = GrovePi(self.config["groveSettings"])
-        self.sqlClient = SQLiteClient()
-        self.mqtt = MqttClient(self.config["mosquittoSettings"])
-        self.formatter = formater.jsonFormater(self.config["location"],self.config["mosquittoSettings"]["clientID"])
 
 
+#TODO:
+# Rewrite mqtt wrapper
+# Add functions for digital and I2C sensors 
+# Add JSON formatter for all values
+# Add Barometer to grovepi lib (Send Pull request to dexterInd)
+#
+#
 
-    def loop(self):
-        while True:
-            sensorData = self.grove.readSensors()
-            json = self.formatter.sensorToJSON(sensorData, time.time())
-            #print sensorData
-            #self.sqlClient.addValues(sensorData)
-            self.mqtt.publish(json)
-            time.sleep(60*self.config["updateIntervall"])
+sensorValues = dict()
+
+analogSensors = dict(
+    light = 0,
+    air = 1,
+)
+digitalSensors = dict(
+)
+
+I2CSensors = dict(
+)
+
+mosquittoSettings = dict(
+    server      = '127.0.0.1',
+    port        = 12000,
+    clientID    = 'TestClient',
+    controlTopic    = 'yay/super/topic',
+    publishTopic    = 'yay/geiles/topic',
+    qos         = 0,
+)
+
+location = dict(
+    lat         = 48.7833,
+    long        = 9.1833,
+)
+
+updateInterval = 15
+
+#Register all analog ensors at the GrovePI
+def initAnalogSensors():
+    for sensor in analogSensors:
+        grovepi.pinMode(analogSensors[sensor],"Input")
+#Read all analog sensors 
+def readAnalogSensors():
+    for sensor in analogSensors:
+        sensorValues[sensor] = grovepi.analogRead(analogSensors[sensor])
+
+initAnalogSensors()
+
+#For now: Print all the analog sensor values
+while True:
+    readAnalogSensors()
+    print sensorValues
+    time.sleep(2)
 
 
-if __name__ == "__main__":
-    client = PoseidonClient()
-    client.loop()
